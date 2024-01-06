8c63d49bfac2b5be90035a8d7efc1c91da87bd57
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-01-07 08:39:42 +0900

    Add category router to main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer
 
-from api import auth, user, blog, comment
+from api import auth, user, blog, comment, category
 
 from common.database import engine
 from common import models
@@ -16,15 +16,11 @@ models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
-    return {"message": "Top page"}
-
-
-@app.get("/setting/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+    return {"message": "Please see `/docs` for usage."}
 
 
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(blog.router)
 app.include_router(comment.router)
+app.include_router(category.router)
