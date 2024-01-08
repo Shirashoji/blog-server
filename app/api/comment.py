5cfa29a61090424af599404ebcad62238119bb63
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-01-08 17:29:38 +0900

    Update API routes and models

--- app/api/comment.py
+++ app/api/comment.py
@@ -14,7 +14,7 @@ router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@router.get("/comment/", response_model=schemas.Comment, tags=["comment"])
+@router.get("/comment/", response_model=list[schemas.Comment], tags=["comment"])
 def read_comments(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     comments = crud.get_comments(db, skip=skip, limit=limit)
     return comments
@@ -34,7 +34,7 @@ async def update_comment(id: int, comment: schemas.CommentCreate, token: str = D
     db_comment = crud.get_comment_by_id(db, comment_id=id)
     if db_comment is None:
         raise HTTPException(status_code=404, detail="Comment not found")
-    if db_comment.owner_id != user.id:
+    if db_comment.user_id != user.id:
         raise HTTPException(
             status_code=405, detail="Not allowed! you are not the owner of this comment")
 
@@ -49,7 +49,7 @@ async def delete_comment(id: int, token: str = Depends(oauth2_scheme), db: Sessi
     db_comment = crud.get_comment_by_id(db, comment_id=id)
     if db_comment is None:
         raise HTTPException(status_code=404, detail="Comment not found")
-    if db_comment.owner_id != user.id:
+    if db_comment.user_id != user.id:
         raise HTTPException(
             status_code=405, detail="Not allowed! you are not the owner of this comment")
 
@@ -68,3 +68,9 @@ def read_comments(blog_id: int, skip: int = 0, limit: int = 100, db: Session = D
 async def create_comment(blog_id: int, comment: schemas.CommentCreate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     user = await auth.get_current_user(token, db)
     db_blog = crud.get_blog_by_id(db, blog_id=blog_id)
+    if db_blog is None:
+        raise HTTPException(status_code=404, detail="Blog not found")
+
+    db_comment = crud.create_comment(
+        db=db, blog_id=blog_id, user_id=user.id, comment=comment)
+    return db_comment
--- app/api/user.py
+++ app/api/user.py
@@ -13,7 +13,7 @@ router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@router.post("/users/", response_model=schemas.User, tags=["users"])
+@router.post("/user/", response_model=schemas.User, tags=["user"], description="Create a new user")
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
@@ -22,13 +22,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@router.get("/users/", response_model=list[schemas.User], tags=["users"])
+@router.get("/user/", response_model=list[schemas.User], tags=["user"], description="Returns a list of users.")
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@router.get("/users/{username}", response_model=schemas.User, tags=["users"])
+@router.get("/user/{username}", response_model=schemas.User, tags=["user"], description="Returns a user. (by username)")
 def read_user(username: str, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=username)
     if db_user is None:
@@ -36,7 +36,15 @@ def read_user(username: str, db: Session = Depends(get_db)):
     return db_user
 
 
-@router.get("/users/me/", response_model=schemas.User, tags=["users"])
+@router.get("/userId/{user_id}", response_model=schemas.User, tags=["user"], description="Returns a user. (by user_id)")
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@router.get("/my-account/", response_model=schemas.User, tags=["user"], description="Returns the current user.")
 async def read_users_me(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     current_user = await get_current_user(token, db)
     return current_user
